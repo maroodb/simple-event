bc8953e2bc21eb3c37ff4b1be9f2cd8b8b3fc7bd
maroodb <marouen.dbouba@gmail.com>
2022-03-08 21:29:25 +0100

    add multiple constructors options

--- src/main/java/com/maroodb/simpleevent/core/SimpleEvent.java
+++ src/main/java/com/maroodb/simpleevent/core/SimpleEvent.java
@@ -1,13 +1,40 @@
 package com.maroodb.simpleevent.core;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public final class SimpleEvent <I> {
 
+    private final static int DEFAULT_POOL_SIZE = 10;
     private final static AtomicInteger indexer = new AtomicInteger(1);
     private final Map<String, Set<Task<I>>> subscriptions = new HashMap<>();
+    private final ExecutorService executorService;
+    private final List<Future<?>> executingFutures = new LinkedList<>();
+
+    public SimpleEvent() {
+        executorService = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE);
+    }
+    public SimpleEvent(int poolSize) {
+        checkPoolSize(poolSize);
+        executorService = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public SimpleEvent(ExecutorService executorService) {
+        checkExecutorService(executorService);
+        this.executorService = executorService;
+    }
+
+    private void checkExecutorService(ExecutorService executorService) {
+        if (executorService.isShutdown()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     public Observable<I> subscribe(String topic, Consumer<I> consumer) {
 
@@ -30,10 +57,13 @@ public final class SimpleEvent <I> {
         if (topicConsumers == null) {
             return;
         }
-
         for (Task<I> task : topicConsumers) {
-            task.execute(message);
+            Future<?> future = executorService.submit(() -> {
+                task.execute(message);
+            });
+            executingFutures.add(future);
         }
+
     }
 
     public void remove(String topic, Task<I> task) {
@@ -42,5 +72,23 @@ public final class SimpleEvent <I> {
             return;
         }
         topicConsumers.remove(task);
+    }
+
+    public boolean thereIsNoActiveTask() {
+        removeTerminatedFutures();
+        return executingFutures.size() == 0;
+    }
+
+    private void removeTerminatedFutures() {
+        Predicate<Future<?>> isCancelled = Future::isCancelled;
+        Predicate<Future<?>> isDone = Future::isDone;
+
+        executingFutures.removeIf(isDone.or(isCancelled));
+    }
+
+    private void checkPoolSize(int poolSize) {
+        if (poolSize < 1) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/com/maroodb/simpleevent/SimpleEventTest.java
+++ src/test/java/com/maroodb/simpleevent/SimpleEventTest.java
@@ -5,9 +5,15 @@ import com.maroodb.simpleevent.core.SimpleEvent;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.awaitility.Awaitility.with;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class SimpleEventTest {
@@ -36,11 +42,16 @@ public class SimpleEventTest {
     public void subscribeAndExecuteConsumerWhenPublishMessage() {
         SimpleEvent<String> simpleEvent = new SimpleEvent<>();
         String message = "Hello World!";
-        StringBuilder sb = new StringBuilder();
+        StringBuffer sb = new StringBuffer();
         Consumer<String> consumer = sb::append;
 
         Observable<String> observable = simpleEvent.subscribe(TOPIC, consumer);
         simpleEvent.publish(TOPIC, message);
+
+        with().pollDelay(100, MILLISECONDS)
+                .and()
+                .pollInterval(200, MILLISECONDS)
+                .await().until(simpleEvent::thereIsNoActiveTask);
 
         assertEquals(message, sb.toString());
 
@@ -50,13 +61,145 @@ public class SimpleEventTest {
     public void subscribeThenUnsubscribeFromAnEvent() {
         SimpleEvent<String> simpleEvent = new SimpleEvent<>();
         String message = "Hello World!";
-        StringBuilder sb = new StringBuilder();
+        StringBuffer sb = new StringBuffer();
         Consumer<String> consumer = sb::append;
 
         Observable<String> observable = simpleEvent.subscribe(TOPIC, consumer);
         observable.unsubscribe();
         simpleEvent.publish(TOPIC, message);
 
+
         assertEquals(0, sb.length());
     }
+
+    @Test
+    public void handleMultipleConsumersOfSameTopic() {
+
+        SimpleEvent<String> simpleEvent = new SimpleEvent<>();
+        String message = "H";
+        StringBuffer sb = new StringBuffer();
+        Consumer<String> consumer1 = sb::append;
+        Consumer<String> consumer2 = sb::append;
+        String expectedResult = "HH";
+
+        Observable<String> observable1 = simpleEvent.subscribe(TOPIC, consumer1);
+        Observable<String> observable2 = simpleEvent.subscribe(TOPIC, consumer2);
+
+        simpleEvent.publish(TOPIC, message);
+
+        with().pollDelay(100, MILLISECONDS)
+                .and()
+                .pollInterval(200, MILLISECONDS)
+                .await().until(simpleEvent::thereIsNoActiveTask);
+
+        assertEquals(sb.toString(), expectedResult);
+    }
+
+   @Test
+    public void handleMultipleConsumersOfSameTopicNonBlocking() {
+
+        SimpleEvent<String> simpleEvent = new SimpleEvent<>();
+        String message = "Hello World!";
+        StringBuffer sb = new StringBuffer();
+        String expectedResult = "21";
+
+        Consumer<String> consumer1 = (s) -> {
+            sb.append("2");
+        };
+        Consumer<String> consumer2 = (s) -> {
+            try {
+                Thread.sleep(500);
+                sb.append("1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Observable<String> observable2 = simpleEvent.subscribe(TOPIC, consumer2);
+        Observable<String> observable1 = simpleEvent.subscribe(TOPIC, consumer1);
+
+        simpleEvent.publish(TOPIC, message);
+
+       with().pollDelay(100, MILLISECONDS)
+               .and()
+               .pollInterval(200, MILLISECONDS)
+               .await().until(simpleEvent::thereIsNoActiveTask);
+
+        assertEquals(sb.toString(), expectedResult);
+    }
+
+    @Test
+    public void defineThreadPoolSize() {
+        SimpleEvent<String> simpleEvent = new SimpleEvent<>(1);
+        String message = "Hello World!";
+        StringBuffer sb = new StringBuffer();
+
+        String expectedResult = "12";
+
+        Consumer<String> consumer1 = (s) -> {
+            sb.append("2");
+        };
+        Consumer<String> consumer2 = (s) -> {
+            try {
+                Thread.sleep(500);
+                sb.append("1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Observable<String> observable2 = simpleEvent.subscribe(TOPIC, consumer2);
+        Observable<String> observable1 = simpleEvent.subscribe(TOPIC, consumer1);
+
+        simpleEvent.publish(TOPIC, message);
+
+        with().pollDelay(100, MILLISECONDS)
+                .and()
+                .pollInterval(200, MILLISECONDS)
+                .await().until(simpleEvent::thereIsNoActiveTask);
+        // given a poolSize = 1, task 2 will wait for task 1 to finish
+        assertEquals(sb.toString(), expectedResult);
+    }
+
+    @Test
+    public void throwExceptionWhenGivenAZeroThreadPoolSize() {
+        SimpleEvent<String> simpleEvent;
+        Supplier<SimpleEvent<String>> supplier = () -> new SimpleEvent<>(0);
+        assertThrows(IllegalArgumentException.class, supplier::get);
+    }
+
+    @Test
+    public void throwExceptionWhenGivenANegativeThreadPoolSize() {
+        SimpleEvent<String> simpleEvent;
+        Supplier<SimpleEvent<String>> supplier = () -> new SimpleEvent<>(-10);
+        assertThrows(IllegalArgumentException.class, supplier::get);
+    }
+
+    @Test
+    public void createSimpleEventWithGivenExecutorService() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        SimpleEvent<String> simpleEvent = new SimpleEvent<String>(executorService);
+        String message = "Hello World!";
+        StringBuffer sb = new StringBuffer();
+        Consumer<String> consumer = sb::append;
+
+        Observable<String> observable = simpleEvent.subscribe(TOPIC, consumer);
+        simpleEvent.publish(TOPIC, message);
+
+        with().pollDelay(100, MILLISECONDS)
+                .and()
+                .pollInterval(200, MILLISECONDS)
+                .await().until(simpleEvent::thereIsNoActiveTask);
+
+        assertEquals(message, sb.toString());
+    }
+
+    @Test
+    public void throwExceptionWhenCreateSimpleEventWithGivenTerminatedExecutorService() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.shutdown();
+        Supplier<SimpleEvent<String>> supplier = () -> new SimpleEvent<>(executorService);
+        assertThrows(IllegalArgumentException.class, supplier::get);
+    }
+
 }
