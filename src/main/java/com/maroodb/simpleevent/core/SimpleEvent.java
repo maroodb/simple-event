aa80b635dcc2a950624c0dc46f245fd3504ddd43
maroodb <marouen.dbouba@gmail.com>
2022-03-10 22:59:01 +0100

    add doc

--- src/main/java/com/maroodb/simpleevent/core/SimpleEvent.java
+++ src/main/java/com/maroodb/simpleevent/core/SimpleEvent.java
@@ -9,7 +9,10 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
- * @author maroodb
+ * SimpleEvent is a PubSub API, which handle subscriptions and publish asynchronously in-memory
+ * using a pool of thread.
+ * @author maroodb (Marouen Dbouba)
+ * @param <I> the type of Message handled by consumers
  */
 public final class SimpleEvent<I> {
 
@@ -20,21 +23,40 @@ public final class SimpleEvent<I> {
     private final ExecutorService executorService;
     private final List<Future<?>> executingFutures = new LinkedList<>();
 
+    /**
+     * Constructs a new <tt>SimpleEvent</tt>.
+     * The <tt>SimpleEvent</tt> is created with a default thread pool size (10)
+     */
     public SimpleEvent() {
         executorService = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE);
     }
 
+    /**
+     * Constructs a new <tt>SimpleEvent</tt> with a specified thread pool size.
+     * @param poolSize the required thread pool size.
+     */
     public SimpleEvent(int poolSize) {
         checkPoolSize(poolSize);
         executorService = Executors.newFixedThreadPool(poolSize);
     }
 
+    /**
+     * Constructs a new <tt>SimpleEvent</tt> with a specified ExecutorService (externally managed)
+     * @param executorService an external managed ExecutorService
+     */
     public SimpleEvent(ExecutorService executorService) {
         checkExecutorService(executorService);
         this.executorService = executorService;
     }
 
+    /**
+     * Subscribe and register the specified consumer to the given topic.
+     * @param topic    a String topic name on which the client want to subscribe.
+     * @param consumer a Consumer lambda function that il will be executed every time a message published to the topic.
+     * @return return an Object of type observable
+     */
     public Observable<I> subscribe(String topic, Consumer<I> consumer) {
+
         checkTopic(topic);
         checkConsumer(consumer);
 
@@ -52,6 +74,11 @@ public final class SimpleEvent<I> {
         return new Observable<>(this, topic, task);
     }
 
+    /**
+     * Publish a message to a specific topic.
+     * @param topic   a String topic name on which the client want to publish
+     * @param message a parameterized message which will be consumed by topic subscribers.
+     */
     public void publish(String topic, I message) {
         runCleanFutureTask();
         Set<Task<I>> topicConsumers = subscriptions.get(topic);
@@ -59,15 +86,14 @@ public final class SimpleEvent<I> {
             return;
         }
         for (Task<I> task : topicConsumers) {
-            Future<?> future = executorService.submit(() -> {
-                task.execute(message);
-            });
+            Future<?> future = executorService.submit(() -> task.execute(message));
             executingFutures.add(future);
         }
 
     }
 
-    public void remove(String topic, Task<I> task) {
+
+    void remove(String topic, Task<I> task) {
         Set<Task<I>> topicConsumers = subscriptions.get(topic);
         if (topicConsumers == null) {
             return;
@@ -75,6 +101,10 @@ public final class SimpleEvent<I> {
         topicConsumers.remove(task);
     }
 
+    /**
+     * Check if there is active thread.
+     * @return a boolean false/true indicates if there is an active running consumer task.
+     */
     public boolean thereIsNoActiveTask() {
         removeTerminatedFutures();
         return executingFutures.size() == 0;
@@ -108,6 +138,7 @@ public final class SimpleEvent<I> {
             throw new IllegalArgumentException();
         }
     }
+
     private void checkConsumer(Consumer<I> consumer) {
         if (consumer == null) {
             throw new IllegalArgumentException();
